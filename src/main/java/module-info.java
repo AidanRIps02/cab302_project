f1a5ada82766888f3d6735c2e4c2766b652ab527
AidanRIps02 <167734187+AidanRIps02@users.noreply.github.com>
2024-09-17 22:38:27 +1000

    Initial Build

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module qut.group83.cab302_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens qut.group83.cab302_project to javafx.fxml;
+    exports qut.group83.cab302_project;
+}
