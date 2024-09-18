14cdf52fddb773bdf65ccd1a9b210dec60359ad3
AidanRIps02 <167734187+AidanRIps02@users.noreply.github.com>
2024-09-18 13:27:13 +1000

    Some Database-UI Functionality

--- src/main/java/qut/group83/cab302_project/HelloController.java
+++ src/main/java/qut/group83/cab302_project/HelloController.java
@@ -3,12 +3,23 @@ package qut.group83.cab302_project;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.sql.Connection;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick()
+    {
+        Connection connection = DatabaseConnection.getInstance();
+
+        UserDAO userDAO = new UserDAO();
+
+        // Retrieve a user by username and password
+        User user = userDAO.getByUsernameAndPassword("John", "John");
+
+        userDAO.close();
+        welcomeText.setText(user.toString());
     }
 }
